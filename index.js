4717912c183822b33cede75b6008e51dac9f68f4
Daniel Dunbar <danielqdunbar@gmail.com>
2018-03-15 22:47:35 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('.target')
+}
+
+function increaseRankBy(n) {
+  const lis = document.getElementById('app').querySelectorAll('.ranked-list')
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (parseInt(lis[i].innerHTML, 10) + n).toString()
+  }
+  return
+}
+
+function deepestChild() {
+  const list = document.getElementsById('grand-node')
+  return
+}
